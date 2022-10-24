edc0491aa4685aa15cfec4d227702f2d02fadaab
Brunonavarrooficial <bnacessoria@gmail.com>
2022-10-24 17:22:30 -0300

    Add-DOM-Script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,65 @@
-console.log('teste');
+class BoxShadowGenerator {
+    constructor(
+        horizontal,
+        horizontalRef,
+        vertical,
+        vertifalRef,
+        blur,
+        blurRef,
+        spread,
+        spreadRef,
+        previewBox,
+        rule,
+        webkitRule,
+        mozRule
+    ) {
+        this.horizontal = horizontal;
+        this.horizontalRef = horizontalRef;
+        this.vertical = vertical;
+        this.vertifalRef = vertifalRef;
+        this.blur = blur;
+        this.blurRef = blurRef;
+        this.spread = spread;
+        this.spreadRef = spreadRef;
+        this.previewBox = previewBox;
+        this.rule = rule;
+        this.webkitRule = webkitRule;
+        this.mozRule = mozRule;
+    }
+}
+
+// Seleção de elementos
+
+const horizontal = document.querySelector('#horizontal');
+const horizontalRef = document.querySelector('#horizontal-value');
+const vertical = document.querySelector('#vertical');
+const vertifalRef = document.querySelector('#vertical-value');
+const blur = document.querySelector('#blur');
+const blurRef = document.querySelector('#blue-value');
+const spread = document.querySelector('#spread');
+const spreadRef = document.querySelector('#spread-value');
+
+const previewBox = document.querySelector('#box');
+
+const rule = document.querySelector('#rule span');
+const webkitRule = document.querySelector('#webkit-rule span');
+const mozRule = document.querySelector('mox-rule span');
+
+const boxShadow = new BoxShadowGenerator(
+    horizontal,
+    horizontalRef,
+    vertical,
+    vertifalRef,
+    blur,
+    blurRef,
+    spread,
+    spreadRef,
+    previewBox,
+    rule,
+    webkitRule,
+    mozRule
+);
+
+console.log(boxShadow);
+
+// Eventos
